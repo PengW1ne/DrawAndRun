5a80a7d19e57268f614122393e28c5866bf2babf
PengW1ne <goofymilkis@gmail.com>
2021-11-29 19:42:06 +0300

    draw line

---
+++ Assets/Scripts/Line.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Line : MonoBehaviour{
+
+    public LineRenderer lineRenderer;
+    [HideInInspector] public List<Vector2> points;
+    [HideInInspector] public int pointsCount = 0;
+
+    private float pointsMinDistance = 0.1f;
+    
+    public void AddPoint(Vector2 newPoint)
+    {
+        if (pointsCount >= 1 && Vector2.Distance(newPoint, GetLastPoint()) < pointsMinDistance)
+        {
+           return; 
+        }
+        points.Add(newPoint);
+        pointsCount++;
+        lineRenderer.positionCount = pointsCount;
+        lineRenderer.SetPosition(pointsCount - 1, newPoint);
+    }
+
+    public Vector2 GetLastPoint()
+    {
+        return lineRenderer.GetPosition(pointsCount - 1);
+    }
+    
+    public void SetPointsMinDistance(float distance)
+    {
+        pointsMinDistance = distance;
+    }
+    
+    public void SetLineWidth(float width)
+    {
+        lineRenderer.startWidth = width;
+        lineRenderer.endWidth = width;
+    }
+}
---
+++ Assets/Scripts/LineDrawner.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class LineDrawner : MonoBehaviour{
+    
+    public GameObject LinePrefab;
+
+    public float LinePointsMinDistance;
+    public float LineWidth;
+
+    public GraphicRaycaster m_Raycaster;
+    PointerEventData m_PointerEventData;
+    public EventSystem m_EventSystem;
+    
+    private Line currentLine;
+    private Camera cam;
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam = Camera.main;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        if (Input.GetMouseButtonDown(0))
+            BeginDraw();
+        
+        
+        
+        if (currentLine != null)
+        {
+            Draw();
+        }
+    }
+
+    void BeginDraw()
+    {
+        currentLine = Instantiate(LinePrefab, this.transform).GetComponent<Line>();
+        
+        currentLine.SetPointsMinDistance(LinePointsMinDistance);    
+        currentLine.SetLineWidth(LineWidth);    
+    }
+    
+    void Draw()
+    {
+        m_PointerEventData = new PointerEventData(m_EventSystem);
+        m_PointerEventData.position = Input.mousePosition;
+        
+        List<RaycastResult> results = new List<RaycastResult>();
+
+        m_Raycaster.Raycast(m_PointerEventData, results);
+        
+        foreach (RaycastResult result in results)
+        {
+            Vector2 mousePosition = m_PointerEventData.position;
+            currentLine.AddPoint(mousePosition);
+        }
+        if (Input.GetMouseButtonUp(0))
+            EndDraw();
+    }
+    
+    void EndDraw()
+    {
+        if (currentLine != null)
+        {
+            if (currentLine.pointsCount < 2)
+            {
+                Destroy(currentLine.gameObject);
+            }
+            else
+            {
+                StartCoroutine(DistributionOfCharacters());
+            }
+        }
+    }
+
+    IEnumerator DistributionOfCharacters()
+    {
+        Debug.Log("Поставить челиков");
+        yield return new WaitForSeconds(0.5f);
+        Destroy(currentLine.gameObject);
+    }
+}
